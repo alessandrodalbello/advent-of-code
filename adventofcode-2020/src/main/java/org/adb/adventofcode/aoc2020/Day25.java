61c47fea44dcb45d91087ceef35cb9a4fa921c16
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2020-12-25 06:54:02 +0100

    Day 25

---
+++ adventofcode-2020/src/main/java/org/adb/adventofcode/aoc2020/Day25.java
@@ -0,0 +1,53 @@
+package org.adb.adventofcode.aoc2020;
+
+import org.adb.adventofcode.Solver;
+import org.adb.adventofcode.io.FileResourceReader;
+
+class Day25 implements Solver {
+
+    private static final String INPUT_FILENAME = "aoc_day25.txt";
+    private static final long GEN_NUMBER = 7;
+    private static final long REM_NUMBER = 20201227;
+
+    private final long cardPubKey;
+    private final long doorPubKey;
+
+    public Day25() {
+        try (FileResourceReader reader = new FileResourceReader(INPUT_FILENAME)) {
+            long[] input = reader.asLongStream().toArray();
+            cardPubKey = input[0];
+            doorPubKey = input[1];
+        }
+    }
+
+    @Override
+    public void solveSilver() {
+        int cardLoopSize = crackLoopSize(cardPubKey);
+        long encryptionKey = calculateEncryptionKey(doorPubKey, cardLoopSize);
+        System.out.printf("The encryption key is %d.%n", encryptionKey);
+    }
+
+    private int crackLoopSize(long pubKey) {
+        int loopSize = 1;
+        long value = GEN_NUMBER;
+        while (value != pubKey) {
+            value = (value * GEN_NUMBER) % REM_NUMBER;
+            loopSize +=1;
+        }
+        return loopSize;
+    }
+
+    private long calculateEncryptionKey(long subject, int loopSize) {
+        long value = subject;
+        do {
+            value = (value * subject) % REM_NUMBER;
+            loopSize--;
+        } while (loopSize > 1);
+        return value;
+    }
+
+    @Override
+    public void solveGold() {
+        System.out.printf("Merry Christmas!!%n");
+    }
+}
